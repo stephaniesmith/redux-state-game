4ac661afdb1c8864a458b3d960bf125b2f42853e
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-06-12 13:31:44 -0700

    added thunk

--- src/store.js
+++ src/store.js
@@ -1,13 +1,20 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
 import { selections } from './components/game/reducers';
 
 const rootReducer = combineReducers({
   selections
 });
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 const store = createStore(
   rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  composeEnhancers(
+    applyMiddleware(
+      thunk
+    )
+  )
 );
 
 export default store;
