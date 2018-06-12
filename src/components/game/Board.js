7fb3adf87b020ece279c5346ef7813d79ae18040
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-06-12 13:32:08 -0700

    added this.props to makeSelection

--- src/components/game/Board.js
+++ src/components/game/Board.js
@@ -8,25 +8,20 @@ import { makeSelection } from './actions';
 class Board extends Component {
 
   static propTypes = {
-    index: PropTypes.number,
+    // index: PropTypes.number,
     player: PropTypes.string.isRequired,
     board: PropTypes.array.isRequired
   };
-
-  // state = {
-  //   board: [' ', ' ', 'X', ' ', ' ', ' ', ' ', ' ', ' ']
-  // };
 
   callSquare = event => {
     event.preventDefault();
     const { player } = this.props;
     const index = event.target.value;
-    makeSelection(index, player);
+    this.props.makeSelection(index, player);
   };
 
   render() {
     const { player, board } = this.props;
-    // const { board } = this.state;
 
     return (
       <div>
@@ -36,9 +31,6 @@ class Board extends Component {
           selectSquare={this.callSquare}
           value={index}
         />)}
-        {/* <div><Square label={board[0]} selectSquare={this.callSquare}/><Square label={board[1]} selectSquare={this.callSquare}/><Square label={board[2]} selectSquare={this.callSquare}/></div>
-        <div><Square label={board[3]} selectSquare={this.callSquare}/><Square label={board[4]} selectSquare={this.callSquare}/><Square label={board[5]} selectSquare={this.callSquare}/></div>
-        <div><Square label={board[6]} selectSquare={this.callSquare}/><Square label={board[7]} selectSquare={this.callSquare}/><Square label={board[8]} selectSquare={this.callSquare}/></div> */}
       </div>
     );
   }
