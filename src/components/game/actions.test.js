2579fb82e9174f07577912954a3a0a6a3c6d8dd2
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-06-13 10:22:40 -0700

    action test

--- src/components/game/actions.test.js
+++ src/components/game/actions.test.js
@@ -1,22 +1,29 @@
 import { makeSelection } from './actions';
-import { SELECTION } from './reducers';
+import { SELECTION, TURN, TALLY_ROUND } from './reducers';
 
 describe('makeSelection action', () => {
   it('dispatches player choice', () => {
     const thunk = makeSelection(3, 'X');
     const dispatch = jest.fn();
-    const getState = () => ({ selections: [' ', ' ', ' ', 'X', ' ', ' ', ' ', ' ', ' '] });
+    const getState = () => ({ selections: ['X', 'X', 'X', ' ', ' ', ' ', ' ', ' ', ' '] });
     thunk(dispatch, getState);
 
     const { calls } = dispatch.mock;
-    expect(calls.length).toBe(2);
+    expect(calls.length).toBe(3);
 
     expect(calls[0][0]).toEqual({
       type: SELECTION,
       payload: { index: 3, player: 'X' }
     });
 
+    expect(calls[1][0]).toEqual({
+      type: TURN
+    });
+
+    expect(calls[2][0]).toEqual({
+      type: TALLY_ROUND,
+      payload: expect.stringMatching('WIN_X')
+    });
+
   });
-
-
 });
