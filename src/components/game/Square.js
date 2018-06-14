aaeadb273fc0d2f13d63b52baeea08ce283c053a
hnrzzle <henrique.j.oliveira1@gmail.com>
2018-06-14 13:03:29 -0700

    buttons disabled if game is over

--- src/components/game/Square.js
+++ src/components/game/Square.js
@@ -1,22 +1,32 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import styles from './Square.css';
+import { connect } from 'react-redux';
+import { getRoundState, ROUND_STATE } from './reducers';
 
-export default class Square extends Component {
+class Square extends Component {
 
   static propTypes = {
     label: PropTypes.string.isRequired,
     selectSquare: PropTypes.func.isRequired,
-    value: PropTypes.number.isRequired
+    value: PropTypes.number.isRequired,
+    roundState: PropTypes.string.isRequired
   };
 
   render() {
-    const { label, selectSquare, value } = this.props;
+    const { label, selectSquare, value, roundState } = this.props;
 
     return (
-      <button className={styles.square} value={value} onClick={selectSquare} disabled={label !== ' '}>
+      <button className={styles.square} value={value} onClick={selectSquare} 
+        disabled={label !== ' ' || roundState !== ROUND_STATE.PLAYING}>
         {label}
       </button>
     );
   }
 }
+
+export default connect(
+  (state) => ({
+    roundState: getRoundState(state)
+  })
+)(Square);
